e834c74d7111f47db542ce9a85c48e52d11d3bca
adsfaesa0508 <adsfaesa0508@gmail.com>
2022-11-26 15:16:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+ ## Swing + Java
+
+##  Como usar calculadora
+
+``` bash
+$ java -jar dist/Calculadora.jar
+```
+
+![imagem](image/Calculadora.png "Imagem do programa")
+
+
---
+++ src/br/com/igor/calculadora/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.igor.calculadora;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import br.com.igor.calculadora.gui.CalculadoraGui;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+	private static void lookAndFeel() {
+
+		try {
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
+
+	}
+
+	public Main() {
+		lookAndFeel();
+		new CalculadoraGui().setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+}
